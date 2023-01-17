dfc1243473bfdb2acd3b463732d991cef22f6442
maykon <nokiam13@hotmail.com>
2023-01-16 21:40:41 -0300

    criação do script.js na pasta public

---
+++ public/script.js
@@ -0,0 +1,10 @@
+var number1 = document.getElementById('number1');
+var number2 = document.getElementById('number2');
+var botao = document.getElementById('calcular');
+var result = document.getElementById('resultado');
+function calcular(n1, n2) {
+    return n1 + n2;
+}
+botao.addEventListener('click', function () {
+    result.innerHTML = calcular(number1.value, number2.value);
+});
