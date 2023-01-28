8eb0f0351ec16abf8e6a446c94e874d6df965ebd
maykon <nokiam13@hotmail.com>
2023-01-28 11:10:15 -0300

    melhora no arquivo.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var number1 = document.getElementById('number1');
+var number1 = document.getElementById('number1'); // types assertions
 var number2 = document.getElementById('number2');
 var botao = document.getElementById('calcular');
 var result = document.getElementById('resultado');
@@ -6,5 +6,5 @@ function calcular(n1, n2) {
     return n1 + n2;
 }
 botao.addEventListener('click', function () {
-    result.innerHTML = calcular(number1.value, number2.value);
+    result.innerHTML = calcular(+number1.value, +number2.value).toString();
 });
